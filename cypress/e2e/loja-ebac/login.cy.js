904fc3ddc96a0f4439adea027236a9d1d372d653
Gabriel Cimadon <gabriel.ncimadon@gmail.com>
2025-10-23 22:04:03 -0300

    transformada funçao 'deve fazer login com sucesso usando fixture' em assincrona

--- cypress/e2e/loja-ebac/login.cy.js
+++ cypress/e2e/loja-ebac/login.cy.js
@@ -39,20 +39,21 @@ describe('Funcionalidade: Login', ( ) => {
     });
 
     it('Deve fazer login com sucesso usando massa de dados', () => {
-        cy.get('#username') .type(perfil.usuario)
+        cy.get('#username').type(perfil.usuario)
         cy.get('#password').type(perfil.senha)
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, gabriel.ncimadon')
         
     });
 
-    it('Deve fazer login com sucesso usando fixture', () => {
-        cy.fixture('perfil').then( dados =>{
-            cy.get('#username') .type(dados.usuario, {log: false})
-            cy.get('#password').type(dados.senha, {log: false})
+    it('Deve fazer login com sucesso usando fixture', async() => {
+        const perfilFixture = await cy.fixture('perfil')
+       
+            cy.get('#username').type(perfilFixture.usuario, {log: false})
+            cy.get('#password').type(perfilFixture.senha, {log: false})
             cy.get('.woocommerce-form > .button').click()
             cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, gabriel.ncimadon')
-        })
+      
 });
 
     it('Deve fazer login com sucesso - usando comandos customizados', () => {
